9659daed3b5b1812a7344af704a007f57704e41f
Jonatan Barcelos <jonatan-barcelos@hotmail.com>
2022-12-15 11:37:38 -0300

    Atualização do código penal

--- script_norm.js
+++ script_norm.js
@@ -455,3 +455,27 @@ setInterval(function(){
         timeout_copy = 0;
     }
 }, 1000);
+
+// PAREI NA SETAGEM DE COOKIE DO ID DA PESSOA
+
+function setCookie(cname, cvalue) {
+    const d = new Date();
+    d.setTime(d.getTime() + 90*24*60*60*1000);
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
